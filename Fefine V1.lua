9a89694a5ab397632c1d5195c431b5993957b50a
xxgalaxyexplosionxx <97001909+xxgalaxyexplosionxx@users.noreply.github.com>
2022-09-18 13:42:59 -0400

    Update Fefine V1.lua

--- Fefine V1.lua
+++ Fefine V1.lua
@@ -12,7 +12,7 @@ Section:NewToggle("AntiVoid", "Prevents you from falling into the void (Abusing 
 		voidjump = Instance.new("Part")
 		voidjump.Parent = workspace
 		voidjump.Name = "AntiVoid"
-		voidjump.Position = Vector3.new(0, 8, 52)
+		voidjump.Position = Vector3.new(0, FallenPartsDestroyHeight + 8, 52)
 		voidjump.Size = Vector3.new(512, 8, 512)
 		voidjump.Anchored = true
 		voidjump.Color = Color3.fromRGB(56, 255, 249)
