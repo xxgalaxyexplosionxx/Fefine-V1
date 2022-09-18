0efa810e56beab7d7d00bf9a843ab58fe7cbc417
xxgalaxyexplosionxx <97001909+xxgalaxyexplosionxx@users.noreply.github.com>
2022-09-18 13:48:31 -0400

    Update Fefine V1.lua

--- Fefine V1.lua
+++ Fefine V1.lua
@@ -7,12 +7,12 @@ local World = Window:NewTab("World")
 
 local Section = World:NewSection("Instances")
 
-Section:NewToggle("AntiVoid", "Prevents you from falling into the void (Abusing may cause lagbacks.)", function(state)
+Section:NewToggle("AntiVoid", "Prevents you from falling into the void (In bedwars, Abusing may cause lagbacks.)", function(state)
 	if state then
 		voidjump = Instance.new("Part")
 		voidjump.Parent = workspace
 		voidjump.Name = "AntiVoid"
-		voidjump.Position = Vector3.new(0, FallenPartsDestroyHeight.Value + 25, 52)
+		voidjump.Position = Vector3.new(0, FallenPartsDestroyHeight, 52)
 		voidjump.Size = Vector3.new(512, 8, 512)
 		voidjump.Anchored = true
 		voidjump.Color = Color3.fromRGB(56, 255, 249)
