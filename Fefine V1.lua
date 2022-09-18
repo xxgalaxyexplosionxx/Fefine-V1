9e16f514b399205b3f60e316872cdcbfe7500e1f
xxgalaxyexplosionxx <97001909+xxgalaxyexplosionxx@users.noreply.github.com>
2022-09-18 17:47:43 -0400

    Update Fefine V1.lua

--- Fefine V1.lua
+++ Fefine V1.lua
@@ -1,6 +1,5 @@
 voidjump = nil
 bounceamount = 50
-antivoidy = -90 
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Fefine V1", "Synapse")
@@ -13,16 +12,12 @@ Instances:NewSlider("Bounce Amount", "How far you bounce when antivoid activates
     bounceamount = s
 end)
 
-Instances:NewSlider("AntiVoid Y", "The Y Position of the antivoid.", 500, -500, function(s)
-    antivoidy = s
-end)
-
 Instances:NewToggle("AntiVoid", "Prevents you from falling into the void (In bedwars, Abusing may cause lagbacks.)", function(state)
 	if state then
 		voidjump = Instance.new("Part")
 		voidjump.Parent = workspace
 		voidjump.Name = "AntiVoid"
-		voidjump.Position = Vector3.new(0, antivoidy, 0)
+		voidjump.Position = Vector3.new(0, game.Players.LocalPlayer.Character.HumanoidRootPart.Position.Y, 0)
 		voidjump.Size = Vector3.new(1000, 8, 1000)
 		voidjump.Anchored = true
 		voidjump.Color = Color3.fromRGB(56, 255, 249)
@@ -33,10 +28,6 @@ Instances:NewToggle("AntiVoid", "Prevents you from falling into the void (In bed
             		end
 		end)
         	voidjump.Transparency = 0.8
-			
-		while wait(0.1) do
-			voidjump.Position = Vector3.new(0, antivoidy, 0)
-		end
 	else
 		game.Workspace.AntiVoid:Destroy()
 	end
